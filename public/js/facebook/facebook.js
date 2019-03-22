668c831f9e69f0870e077e15117d28ed093e742b
jhynzar <jhynzar@gmail.com>
2019-03-22 09:46:18 +0800

    [FB API] moved to single script file

---
+++ public/js/facebook/facebook.js
@@ -0,0 +1,44 @@
+
+/**
+ * FACEBOOK API INITIALIZATION
+ */
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '580499089083300',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/**
+ * FACEBOOK SHARER
+ */
+
+function facebookShareOverrideOGMeta(overrideLink, overrideTitle, overrideDescription, overrideImage)
+{
+    FB.ui({
+        method: 'share_open_graph',
+        action_type: 'og.likes',
+        action_properties: JSON.stringify({
+            object: {
+                'og:url': overrideLink,
+                'og:title': overrideTitle,
+                'og:description': overrideDescription,
+                'og:image': overrideImage
+            }
+        })
+    },
+    function (response) {
+    // Action after response
+    });
+}
