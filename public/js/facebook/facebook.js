3eb187c39dd7e75768997d2d926e511860a06cf6
jhynzar <jhynzar@gmail.com>
2019-03-22 10:34:25 +0800

    [FB API] FB API version upgrade

--- public/js/facebook/facebook.js
+++ public/js/facebook/facebook.js
@@ -4,21 +4,20 @@
  */
 window.fbAsyncInit = function() {
     FB.init({
-      appId            : '580499089083300',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.10'
+      appId      : '580499089083300',
+      xfbml      : true,
+      version    : 'v3.2'
     });
     FB.AppEvents.logPageView();
-};
+  };
 
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 /**
  * FACEBOOK SHARER
